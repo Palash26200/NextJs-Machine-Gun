e43899e4568a3b6b9552050ff9c31f2a78424111
palash sharma <palashsharma259@gmail.com>
2024-01-07 07:54:22 -0600

    Created Navbar and hero section

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,49 @@
+"use client"
+import { Button } from '@/components/ui/button';
+import Link from 'next/link'
+import { usePathname } from 'next/navigation';
+import { Laptop } from 'lucide-react'
+
+const links = [
+    {name: "Home", href: "/"},
+    {name: "Assets", href: "/assets"},
+    {name: "Employees", href: "/employees"}
+]
+
+const Navbar = () => {
+    const pathname = usePathname();
+  return (
+    <div className='mb-8 border-b'>
+      <div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
+        <Link href={'/'}>
+            <h1 className="text-4xl font-bold">FinUn<span className='text-primary'>ityPro</span></h1>
+        </Link>
+
+        <nav className="hidden gap-12 lg:flex 2xl:ml-16">
+            {links.map((link, idx) => (
+                <div key={idx}>
+                    {pathname === link.href ? (
+                        <Link className='text-lg font-semibold text-primary' href={link.href}>
+                            {link.name}
+                        </Link>
+                    ) : (
+                        <Link className='text-lg font-semibold text-gray-600 transition duration-100 hover:text-primary' href={link.href}>
+                            {link.name}
+                        </Link>
+                    )}
+                </div>
+            ))}
+        </nav>
+
+        <div className="flex divide-x border-r sm:border-l">
+            <Button variant={'outline'} className='flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none'>
+                <Laptop />
+                <span className='hidden text-xs font-semibold text-gray-500 sm:block'>My Asset</span>
+            </Button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import Hero from "./components/Hero";
+
 export default function Home() {
   return (
-    <div>Welcome to NextJs Machine gun</div>
+    <div className="bg-white pb-6 sm:pb-8 lg:pb-12">
+      <Hero/>
+    </div>
   )
 }
