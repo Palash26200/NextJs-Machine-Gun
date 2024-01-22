58943bfd5426d346d78081d800bed9ce32c0b3ae
palash sharma <palashsharma259@gmail.com>
2024-01-22 10:36:09 -0600

    adding layout

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -1,8 +1,6 @@
 import AssetCards from "@/components/AssetCards"
 import Hero from "@/components/Hero"
 
-export const prefixUrl = "/web"
-
 export default function Home() {
   return (
     <div className="bg-white pb-6 sm:pb-8 lg:pb-12">
